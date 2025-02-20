0a9ae363a8d43d75e7e07ba99d7ae0638d53bb54
橘子不是唯一的水果 <90956796+lixie021@users.noreply.github.com>
2025-02-20 19:55:00 +0800

    Auto update: 2025-02-20 19:55:00

--- github_push.sh
+++ github_push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# pwd
+cd /Users/beiyiwangdejiyi/Desktop/lixie-work/lvycoder.github.io/
+
 # 获取当前时间作为提交信息
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 
@@ -14,7 +17,7 @@ if [[ -n $(git status -s) ]]; then
     git commit -m "Auto update: $current_time"
 
     # 推送到远程仓库
-    if git push origin master; then
+    if git push origin lixie -f; then
         echo "成功推送到 GitHub!"
     else
         echo "推送失败,请检查网络或权限设置"
